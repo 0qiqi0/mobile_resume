c25a38cdcc193b477d7537e8f338f9a883fabc8e
qiqi <1483593015@qq.com>
2017-12-10 22:28:17 +0800

    work experience

--- js/index.js
+++ js/index.js
@@ -110,7 +110,6 @@ var phoneRender=(function(){
             //给listern中的touch绑定单击事件,移动端的单击事件使用click,类似的有touchstart,touchmove,touchend来模拟.
             //$listenTouch.singleTap(function(){
             $listenTouch.click(function(){
-                console.log('111',$listenTouch)
                 $listen.css('display','none');
                 listenMusic.pause();
 
@@ -138,7 +137,7 @@ var messageRender = (function(){
         step = -1,
         total = $list.length,
         bounceTop =0;
-    var messageMusci = $('#messageMusic');
+    var messageMusic = $('#messageMusic')[0];  //play is not a function 解决办法
 
     /*--s实现消息列表一条条发送--*/
     function messageMove(){
@@ -166,10 +165,10 @@ var messageRender = (function(){
             if(step===total-1){
                 window.clearInterval(autoTimer);
                 window.setTimeout(function(){
-                    if(page ===2) return;
+                    //if(page ===2) return;
 
                     $message.css('display','none');
-                    messageMusci.pause();
+                    messageMusic.pause();
                     cubeRender.init();
 
                 },1500);
@@ -179,7 +178,7 @@ var messageRender = (function(){
     }
     //实现文字打印
     function textMove(){
-        var text = '前端三大件,react技术栈,mobileweb',
+        var text = '你有哪些擅长的前端技能呢?',
             n = -1,
             result = '';
         var textTime =window.setInterval(function(){
@@ -200,19 +199,115 @@ var messageRender = (function(){
         init:function(){
             $message.css('display','block');
             messageMove();
-            messageMusci.play();
+
+            messageMusic.play();
         }
     }
 })()
 
 /*魔方区域*/
 var cubeRender =(function(){
+    var $cube = $('.cube'),
+        $cubebox = $cube.children('.cubebox'),
+        $cubeBoxList  = $cubebox.children('li');
+    console.log('=====',$cubebox)
+
+    //判断滑动和点击里的临界点
+    function isSwipe(changeX,changeY){
+        return Math.abs(changeX)>30 || Math.abs(changeY)>30;
+    }
+
+    // 滑动的处理
+    function start(ev){
+        console.log('eeeee',$cubebox.attr('rotateX'))
+        var point = ev.touches[0];  //记录多个手指操作的信息
+        $(this).attr({
+            strX:point.clientX,
+            strY:point.clientY,
+            changeX:0,
+            changY:0
+        })
+    }
+
+    function  move(ev){
+
+        var point = ev.touches[0],
+            changeX = point.clientX-$(this).attr('strX'), //strXY在start函数中存了
+            changeY = point.clientY - $(this).attr('strY');
+        $(this).attr({
+            changeX:changeX,
+            changeY:changeY
+        })
+        //console.log('----',$(this).attr('changeX'));
+    }
+
+    function end(){
+        var changeX = parseFloat($(this).attr('changeX')),
+            changeY = parseFloat($(this).attr('changeY'));
+
+        var rotateX = parseFloat($(this).attr('rotateX')),
+            rotateY = parseFloat($(this).attr('rotateY'))
+        if(isSwipe(changeX,changeY)===false) return;
+        rotateX = rotateX - changeY / 2;
+        rotateY = rotateY + changeX / 2;
+        $(this).attr({
+            rotateX:rotateX,
+            rotateY:rotateY
+        }).css('transform',' scale(0.6) rotateX('+rotateX+'deg) rotateY('+rotateY+'deg)')
+
+
+    }
     return {
         init:function(){
+            $cube.css('display','block');
 
+            //魔方区域的滑动
+            $cubebox.attr({
+                rotateX: -35,
+                rotateY: 45
+            }).on('touchstart',start).on('touchmove',move).on('touchend',end);
+
+            //每个页面的点击操作
+            //$cubebox.singleTap(function(){
+            $cubebox.click(function(){
+                var index = $(this).index();
+                $cube.css('display','none');
+                swiperRender.init(index);
+            })
         }
     }
 })()
+
+
+/*SWIPER*/
+
+var swiperRender = (function(){
+    var $swiper = $ ('#swiper')
+    return{
+        init:function(index){
+            $swiper.css('display','block');
+
+            console.log('vvvv',new Swiper('.swiper-container'))
+
+            //初始化swiper实现6个页面之间的切换.   1参:目标区域;2参:配置
+            var mySwiper = new Swiper('.swiper-container',{
+                effect : "coverflow",
+
+                onTransitionEnd : function(example){
+                    //不管切换到下一区域还是有回到本区域,会触发swiper内置css3的电话,同样也会触发onTansitionEnd的回调
+                    //example:是我们当前创建的swiper这个类的实例
+                    //example.slides: 记录了当前所有的slide切换的块
+                    //example.activeIndex:记录了当前正在展示的这一活动块的索引.constructor
+
+                }
+            });
+            index = index ||0;
+            console.log('ppp',mySwiper);
+            mySwiper.slideTo(index,2);
+
+        }
+    }
+})();
 
 var urlObj = window.location.href.queryURLParameter(),
     page = parseFloat(urlObj['page']);
@@ -232,5 +327,14 @@ if(page ===2){
 if(page ===3){
     cubeRender.init();
 }
+
+if(page ===4){
+    swiperRender.init();
+}
+
+
+
+
+
 
 
